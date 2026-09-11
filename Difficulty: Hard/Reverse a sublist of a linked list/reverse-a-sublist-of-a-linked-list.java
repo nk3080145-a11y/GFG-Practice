cbcf5a5167e16ab77da92196383b5bf2929cad77
Noman Khan <nk3080145@gmail.com>
2026-09-11 12:38:37 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Hard/Reverse a sublist of a linked list/reverse-a-sublist-of-a-linked-list.java
@@ -0,0 +1,23 @@
+class Solution {
+    Node reverseBetween(int a, int b, Node head) {
+        ArrayList<Node> arr = new ArrayList<>();
+        Node temp = head;
+        while(temp!=null){
+            arr.add(temp);
+            temp = temp.next;
+        }
+        int i = a-1 , j = b - 1;
+        while(i<j){
+            Node t1 = arr.get(i);
+            Node t2 = arr.get(j);
+            arr.set(i,t2);
+            arr.set(j,t1);
+            i++;
+            j--;
+        }
+        for(i=0;i<arr.size();i++){
+            arr.get(i).next = (i==arr.size()-1) ? null : arr.get(i+1);
+        }
+        return arr.get(0);
+    }
+}
